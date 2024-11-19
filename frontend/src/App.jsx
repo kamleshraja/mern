c64ee07bad3770c16f6784ae62104c6742c188fa
kamlesheleva8e <kamlesh.paswan@eleva8e.com>
2024-11-19 13:19:00 +0530

    Added frontend with backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+    const [jokes, setjokes] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/api/jokes")
+            .then((response) => setjokes(response.data))
+            .catch((error) => console.log(error));
+    });
+
+    return (
+        <>
+            <h1>Hello</h1>
+            <p>JOKES: {jokes.length}</p>
+
+            {jokes?.map((joke, index) => (
+                <div key={joke?.id}>
+                    <p>{joke?.title}</p>
+                    <p>{joke?.content}</p>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default App;
